71e75811653bc3178aa0ae9a88c7cae2465957bb
User <User@WIN-EQO5781G9M2>
2024-10-24 20:25:59 +0300

    ???

--- budget/models.py
+++ budget/models.py
@@ -31,7 +31,7 @@ class AcccountEntry(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
-    category_id = models.Model(Category,on_delete=models.CASCADE, related_name='category')
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
 
     def __str__(self):
         return self.id
