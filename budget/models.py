b51ce0698212ed9b02c5cfd3bb60338268399941
User <User@WIN-EQO5781G9M2>
2024-10-24 19:59:07 +0300

    added a new model and connection

--- budget/models.py
+++ budget/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 
 class Account(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,3 +15,15 @@ class Account(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class AcccountEntry(models.Model):
+    id = models.AutoField(primary_key=True)
+    debit_account_id = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='debit_account')
+    credit_account_id = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='credit_account')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.id
