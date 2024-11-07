f4cee51d4dc40d9325413d39fa3c57b0cc462292
User <User@WIN-EQO5781G9M2>
2024-11-07 20:30:43 +0300

    fixed login

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path 
-from ..budget import views
+from budget import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.login),
-
+    path('login/', views.login)
 ]
