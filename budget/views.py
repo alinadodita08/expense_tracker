8edfc5d756fe8f1e60364adbf08c7602560ee20b
User <User@WIN-EQO5781G9M2>
2024-11-07 21:13:33 +0300

    building a register page

--- budget/views.py
+++ budget/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+
 def login(request):
     return render(request, 'login.html')
 
+# GET, POST
 def register(request):
-    return render(request,'register.html')
+    if request.method == 'POST':
+        password = request.POST['password']
+        password_repeat = request.POST['password-repeat']
+        if password != password_repeat:
+            return render(request, 'register.html', {'error': 'Passwords do not match'})
+
+    return render(request, 'register.html')
